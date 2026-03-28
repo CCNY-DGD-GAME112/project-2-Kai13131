742d1cf7015cb10a5a1237842f2f2bfb23a01429
Kai13131 <haiyang.he13@gmail.com>
2026-03-28 00:22:03 -0400

    3.28

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,21 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController player;
+    public Camera shoulder;
+    public GameObject gun;
+    public Rigidbody RB;
+    public Projectile3DController ProjectilePrefab;
+
+    public float MouseSensitivity = 3;
+    public float WalkSpeed = 10;
+    public float JumpPower = 7;
+
+    public List<GameObject> Floors;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        player = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 dir = new Vector3(horizontal,0,vertical);
-        Debug.DrawRay(transform.position, dir, Color.red);
-        player.SimpleMove(dir * 2);
+        cameraMove();
+        playerMove();
+
+        gun.transform.forward = shoulder.transform.forward;
+        gun.transform.rotation = shoulder.transform.rotation;
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            //Spawn a projectile right in front of my eyes
+            Instantiate(ProjectilePrefab, gun.transform.position + gun.transform.forward,
+                gun.transform.rotation);
+        }
+    }
+
+    void cameraMove()
+    {
+        //If my mouse goes left/right my body moves left/right
+        float xRot = Input.GetAxis("Mouse X") * MouseSensitivity;
+        transform.Rotate(0, xRot, 0);
+
+        //If my mouse goes up/down my aim (but not body) go up/down
+        float yRot = -Input.GetAxis("Mouse Y") * MouseSensitivity;
+        shoulder.transform.Rotate(yRot, 0, 0);
+    }
+
+    void playerMove()
+    {
+        if (WalkSpeed > 0)
+        {
+            //My temp velocity variable
+            Vector3 move = Vector3.zero;
+
+            //transform.forward/right are relative to the direction my body is facing
+            if (Input.GetKey(KeyCode.W))
+                move += transform.forward;
+            if (Input.GetKey(KeyCode.S))
+                move -= transform.forward;
+            if (Input.GetKey(KeyCode.A))
+                move -= transform.right;
+            if (Input.GetKey(KeyCode.D))
+                move += transform.right;
+            //I reduce my total movement to 1 and then multiply it by my speed
+            move = move.normalized * WalkSpeed;
+
+            //If I hit jump and am on the ground, I jump
+            if (JumpPower > 0 && Input.GetKeyDown(KeyCode.Space) && OnGround())
+                move.y = JumpPower;
+            else  //Otherwise, my Y velocity is whatever it was last frame
+                move.y = RB.linearVelocity.y;
+
+            //Plug my calculated velocity into the rigidbody
+            RB.linearVelocity = move;
+        }
+    }
+
+    public bool OnGround()
+    {
+        return Floors.Count > 0;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //If I touch something and it's not already in my list of things I'm touching. . .
+        //Add it to the list
+        if (!Floors.Contains(other.gameObject))
+            Floors.Add(other.gameObject);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        //When I stop touching something, remove it from the list of things I'm touching
+        Floors.Remove(other.gameObject);
     }
 }
